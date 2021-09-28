fdc2991bbb8a2c13cb8c229ec2df4e8e66f6c213
황복실 <tlatms0908@gmail.com>
2021-09-29 01:32:43 +0900

    Add: 랜덤 게임 기능 구현

--- assignment7/src/index.js
+++ assignment7/src/index.js
@@ -1,7 +1,24 @@
 const generateNumber = document.querySelector("#gener-num input");
+const guessNumber = document.querySelector("#geuss-num input:first-child");
+const playBtn = document.querySelector("#geuss-num #play-btn");
+const matchNumber = document.querySelector("#match");
+const resultNumber = document.querySelector("#result");
+
+const generNum = generateNumber.value;
+const guessNum = guessNumber.value;
 
 function handleInputNumber() {
     console.log(generateNumber.value);
 }
 
+function handleClickBtn() {
+    matchNumber.innerTEXT = `You chose: ${generNum}, the machine chose: ${guessNum}`;
+    if (generNum !== guessNum) {
+        resultNumber.innerTEXT = "You lost!!";
+    } else {
+        resultNumber.innerTEXT = "You won!!";
+    }
+}
+
 generateNumber.addEventListener("input", handleInputNumber);
+playBtn.addEventListener("click", handleClickBtn);
