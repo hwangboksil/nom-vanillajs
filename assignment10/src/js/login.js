7a30f03ab32033aa2cacbaf3bac2c83509ff1a9a
황복실 <tlatms0908@gmail.com>
2021-10-03 23:08:28 +0900

    Add: 로그인&로그아웃 기능 구현

---
+++ assignment10/src/js/login.js
@@ -0,0 +1,33 @@
+const input = document.querySelector("#login-form");
+const inputName = document.querySelector("#login-form input:first-child");
+const loadName = document.querySelector("#greeting");
+const nameDelete = document.querySelector("#name-del");
+
+const userName = localStorage.getItem("username");
+
+const HIDDEN_INPUT = 'hidden';
+
+function saveName(userName) {
+    localStorage.setItem("username", userName);
+}
+
+function handleSubmitName() {
+    const userInputName = inputName.value;
+    saveName(userInputName);
+}
+
+if (userName !== null) {
+    input.className = HIDDEN_INPUT;
+    loadName.innerText = `Hi, ${userName} :D`;
+} else {
+    input.classList.remove(HIDDEN_INPUT);
+    nameDelete.className = HIDDEN_INPUT;
+}
+
+function handleClickNameDel() {
+    localStorage.removeItem("username");
+    location.reload();
+}
+
+input.addEventListener("submit", handleSubmitName);
+nameDelete.addEventListener("click", handleClickNameDel);
