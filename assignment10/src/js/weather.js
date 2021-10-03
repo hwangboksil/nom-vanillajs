39e95dfe5bf3cb5fc2b37b60b12137770197f72c
황복실 <tlatms0908@gmail.com>
2021-10-04 02:23:01 +0900

    Add: 날씨 정보 받기 기능 구현

---
+++ assignment10/src/js/weather.js
@@ -0,0 +1,24 @@
+const API_KEY = 'ee0106d1351f040736a2ee075c4ed63a';
+
+function onGeoOk(pos) {
+    const lat = pos.coords.latitude;
+    const lon = pos.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=kr&units=metric&appid=${API_KEY}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const city = document.querySelector("#weather span:first-child");
+            const weather = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = data.weather[0].description
+            console.dir(data);
+        });
+}
+
+function onGeoError() {
+    alert("사용자의 위치 정보를 가져올 수 없습니다.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
+// console.dir(navigator.geolocation);
